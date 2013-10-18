4f8f12cb98ef3516567acb5194c05d4fd8e9c93b
rick-qiu <qiudejun@gmail.com>
2013-10-18 08:13:57 +0000

    more coding on linked list

--- linkedlist.cxx
+++ linkedlist.cxx
@@ -1,4 +1,6 @@
 
+#include "linkedlist.hxx"
+
 #include <cstdio>
 #include <iostream>
 #include <vector>
@@ -9,80 +11,26 @@ using namespace std;
 
 namespace algods {
     namespace linkedlist {
-
-        template<typename T>
-        struct slnode {
-            slnode<T>(const T& d, slnode<T>* n): data(d), next(n) {
+        namespace unittest {
+            int test0() {
+                cout << "running test0, sanity testing" << endl;
+                vector<int> v{1,2,3,4,5,6,7};
+                cout << "single linked list..." << endl;
+                slnode<int>* slhead = nullptr;
+                build_linkedlist(v.begin(), v.end(), slhead);
+                print_linkedlist(slhead);
+                destory_linkedlist(slhead);
+                cout << "double linked list..." << endl;
+                dlnode<int> *dlhead = nullptr;
+                build_linkedlist(v.begin(), v.end(), dlhead);
+                print_linkedlist(dlhead);
+                return 0;
             }
-            T data;
-            slnode* next;
-        };
-
-        template<typename T>
-        struct dlnode {
-            dlnode<T>(const T& d, dlnode<T>* p, dlnode<T>* n) : data(d), prev(p), next(n)
-            T data;
-            dlnode* prev;
-            dlnode* next;
-        };
-
-        template<typename input_iterator>
-        void build_linkedlist(input_iterator begin, input_iterator end,
-                              slnode<typename iterator_traits<input_iterator>::value_type>*& head) {
-            typedef typename iterator_traits<input_iterator>::value_type value_type;
-            typedef slnode<value_type> node_t;
-            node_t **current = &head;
-            while(begin != end) {
-                *current = new node_t(*begin, nullptr);
-                current = &((*current)->next);
-                ++begin;
-            }
-        }
-
-        template<typename input_iterator>
-        void build_linkedlist(input_iterator begin, input_iterator end,
-                              dlnode<typename iterator_traits<input_iterator>::value_type>*& head) {
-            typedef typename iterator_traits<input_iterator>::value_type value_type;
-            typedef dlnode<value_type> node_t;
-            node_t *prev = nullptr;
-            node **current = &head;
-            while(begin != end) {
-                *current = new node_t(*begin, prev, nullptr);
-                prev = *current;
-                current = &((*current)->next);
-                ++begin;
-            }
-            (*current)->next = head;
-            head->prev = (*current);
-        }
-
-        template<typename T>
-        void destory_linkedlist(slnode<T>*& head) {
-            while(head != nullptr) {
-                auto p = head;
-                head = head->next;
-                delete p;
-            }
-        }
-
-        template<typename T>
-        void print_linkedlist(const slnode<T>* head) {
-            while(head != nullptr && head->next != nullptr) {
-                cout << head->data << "->";
-                head = head->next;
-            }
-            cout << head->data << endl;
         }
     }
 }
 
-using namespace algods::linkedlist;
-
 int main(int argc, char* argv[]) {
-    vector<int> v{1,2,3,4,5,6,7};
-    slnode<int>* head = nullptr;
-    build_linkedlist(v.begin(), v.end(), head);
-    print_linkedlist(head);
-    destory_linkedlist(head);
-    return 0;
+    using namespace algods::linkedlist::unittest;
+    return test0();
 }
---
+++ linkedlist.hxx
@@ -0,0 +1,107 @@
+
+#include <iostream>
+#include <iterator>
+
+namespace algods {
+    namespace linkedlist {
+
+        using namespace std;
+
+        template<typename T>
+        struct slnode {
+            slnode<T>(const T& d, slnode<T>* n): data(d), next(n) {
+            }
+            T data;
+            slnode* next;
+        };
+
+        template<typename T>
+        struct dlnode {
+            dlnode<T>(const T& d, dlnode<T>* p, dlnode<T>* n) : data(d), prev(p), next(n) {
+            }
+            T data;
+            dlnode* prev;
+            dlnode* next;
+        };
+
+        template<typename input_iterator>
+        void build_linkedlist(input_iterator begin, input_iterator end,
+                              slnode<typename iterator_traits<input_iterator>::value_type>*& head) {
+            typedef typename iterator_traits<input_iterator>::value_type value_type;
+            typedef slnode<value_type> node_t;
+            node_t **current = &head;
+            while(begin != end) {
+                *current = new node_t(*begin, nullptr);
+                current = &((*current)->next);
+                ++begin;
+            }
+        }
+
+        template<typename input_iterator>
+        void build_linkedlist(input_iterator begin, input_iterator end,
+                              dlnode<typename iterator_traits<input_iterator>::value_type>*& head) {
+            typedef typename iterator_traits<input_iterator>::value_type value_type;
+            typedef dlnode<value_type> node_t;
+            node_t *prev = nullptr;
+            node_t **current = &head;
+            while(begin != end) {
+                *current = new node_t(*begin, prev, nullptr);
+                prev = *current;
+                current = &((*current)->next);
+                ++begin;
+            }
+            if(head != nullptr) {
+                *current = head;
+                head->prev = prev;
+            }
+        }
+
+        template<typename T>
+        void destory_linkedlist(slnode<T>*& head) {
+            while(head != nullptr) {
+                auto p = head;
+                head = head->next;
+                delete p;
+            }
+        }
+
+        template<typename T>
+        void destroy_linkedlist(dlnode<T>*& head) {
+            auto pivot = head;
+            while(pivot != nullptr && pivot->next != head) {
+                auto p = pivot;
+                pivot = pivot->next;
+                delete p;
+            }
+            if(pivot != nullptr) {
+                delete pivot;
+            }
+            head = nullptr;
+        }
+
+        template<typename T>
+        void print_linkedlist(const slnode<T>* head) {
+            while(head != nullptr && head->next != nullptr) {
+                cout << head->data << "->";
+                head = head->next;
+            }
+            if(head != nullptr) {
+                cout << head->data;
+            }
+            cout << endl;
+        }
+
+        template<typename T>
+        void print_linkedlist(const dlnode<T>* head) {
+            auto pivot = head;
+            while(pivot != nullptr && pivot->next != head) {
+                cout << pivot->data << "<->";
+                pivot = pivot->next;
+            }
+            if(pivot != nullptr) {
+                cout << pivot->data;
+            }
+            cout << endl;
+        }
+    }
+}
